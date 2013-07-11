0ad067a155aadee8279e6496dec69ad50051d43a
Chris Moore <christopher.e.moore@gmail.com>
2013-07-10 22:34:10 -0600

    got it to at least get the stupid object, now to keep the stupid run from crashing

--- launch_js.lua
+++ launch_js.lua
@@ -1,7 +1,21 @@
+-- macro for running and testing the js launcher through lua
+--USE_LUA = true
+
+if not USE_LUA then
+	package.path = package.path .. ';./?/init.lua'
+end
+
 launcher = {
 	-- is this supposed to be blocking?
 	getInput = function(callback)
-		-- TODO read something buffered via the js.whatever API
+		if USE_LUA then
+			os.execute("stty raw -echo")	-- read single keystrokes
+			local ch = io.stdin:read(1)
+			os.execute("stty sane")			-- reset read
+			return ch
+		else
+			return js.global.getLastKey()	-- read something buffered via the js API
+		end
 	end,
 }
 
@@ -11,19 +25,26 @@ local col = 1
 local row = 1
 con = {
 	init = function(width_, height_) 
+--print('con.init('..width..', '..height..')')
 		width = width_
 		height = height_
 		con.clear()
 	end,
 	draw = function() 
-		print('drawing!!!')
-		js.outputElement.value = lines:concat('\n')
+--print('con.draw()')
+		if USE_LUA then
+			print(table.concat(lines, '\n'))
+		else
+			js.global.setOutput(table.concat(lines, '\n'))
+		end
 	end,
 	locate = function(x,y) 
+--print('con.locate('..x..','..y..')')
 		col = x
 		row = y
 	end,
 	write = function(s) 
+--print('con.write('..--[[s..--]]')')
 		local line = lines[row]
 		if not line then return end
 		local lhs = line:sub(1,col-1)
@@ -32,6 +53,7 @@ con = {
 		lines[row] = line
 	end,
 	clearline = function()
+--print('con.clearline()')
 		local line = lines[row]
 		if not line then return end
 		local lhs = line:sub(1,col-1)
@@ -39,10 +61,17 @@ con = {
 		lines[row] = line
 	end,
 	clear = function() 
+--print('con.clear()')
 		lines = {}
 		for y=1,height do
 			lines[y] = (' '):rep(width)
 		end
 	end,
 }
+
+print('getting stupid')
+local stupid = require 'stupid'
+print('running stupid')
+stupid.run()
+print('done')
 
--- launch_text.lua
+++ launch_text.lua
@@ -7,30 +7,22 @@ launcher = {
 	end,
 }
 
-con = {}
-
-function con.init()
-end
-
-function con.draw()
-end
-
-function con.locate(x,y)
-	io.write('\027['..y..';'..x..'H')
-end
-
-function con.write(s)
-	io.write(s)
-end
-
-function con.clearline()
-	io.write('\027[K')
-end
-
-function con.clear()
-	io.write('\027[2J')
-end
+con = {
+	init = function() end,
+	draw = function() end,
+	locate = function(x,y)
+		io.write('\027['..y..';'..x..'H')
+	end,
+	write = function(s)
+		io.write(s)
+	end,
+	clearline = function()
+		io.write('\027[K')
+	end,
+	clear = function()
+		io.write('\027[2J')
+	end,
+}
 
 local stupid = require 'stupid'
-
 stupid.run()
